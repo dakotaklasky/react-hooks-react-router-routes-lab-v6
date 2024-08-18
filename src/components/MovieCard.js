7efc5720559f7a296d7d13e925d675ff1be4b57e
Dakota <dakota.klasky@gmail.com>
2024-08-18 13:16:31 -0400

    route functionality

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,11 @@
 import {Link} from 'react-router-dom';
 
-function MovieCard({title}) {
+function MovieCard({movie}) {
   
   return (
     <article>
-        <h2>{title}</h2>
-        {/* What should go here? */}
+        <h1>{movie.title}</h1>
+        <Link to={`/movie/${movie.id}`}>View Info</Link>
     </article>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,36 @@
+import NavBar from "../components/NavBar"
+import MovieCard from "../components/MovieCard"
+
 import { useEffect, useState } from "react";
 
 function Home() {
+
+const [movies, setMovies] = useState([])
+
+
+
+useEffect( () => {
+  fetch("http://localhost:4000/movies")
+  .then(data => data.json())
+  .then(json => setMovies(json))
+}, [])
+
+
+
+
+
+const movieList = movies.map(movie => {
+  return <MovieCard movie={movie} key={movie.id}/>
+})
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar/>
       </header>
       <main>
-        {/* Info goes here! */}
+        <h1>Home Page</h1>
+        {movieList}
       </main>
     </>
   );
